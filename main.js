11853a99654e7db70249c7f16edaa9519066980d
ArbajAr7 <arbajrmacr786@gmail.com>
2024-08-26 22:46:41 +0530

    first commit

--- main.js
+++ main.js
@@ -8,7 +8,25 @@ const rl = readline.createInterface({
 
 rl.question('Enter the filename: ', (filename) => {
   rl.question('Enter the word to remove: ', (word) => {
-    // TODO: Implement this function
+    fs.readFile(filename, 'utf8', (err, data) => {
+      if (err) {
+        console.error(`Error reading file: ${err.message}`);
+      } else {
+
+        // Remove the word from the content
+        const modifiedContent = data.split(word).join('');
+
+        // Optionally, save the modified content back to the file
+        fs.writeFile(filename, modifiedContent, (err) => {
+          if (err) {
+            console.error(`Error writing to file: ${err.message}`);
+          } else {
+            console.log(`Successfully removed "${word}" and saved the file.`);
+            console.log('Modified content: '+ modifiedContent);
+          }
+        });
+      }
+    });
     rl.close();
   });
 });
